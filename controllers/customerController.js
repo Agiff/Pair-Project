956605ab66b4f39bb9145be7486e9d6768f2e013
mfahriferdiansyah <xkadzuvic@gmail.com>
2023-02-10 02:42:45 +0700

    +cart

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,6 @@
-const { User, UserDetail, Product, Category, Transaction } = require('../models');
+const { User, UserDetail, Product, Category, Transaction, Cart, CartProduct } = require('../models');
 const { currencyFormat } = require('../helper');
-const { isCustomer } = require('../middlewares');
+const { isCustomer, isLoggedIn } = require('../middlewares');
 const { Op } = require('sequelize');
 
 class CustomerController {
@@ -28,6 +28,66 @@ class CustomerController {
         res.render('customerDetail', { customer, userId, role});
       })
       .catch(err => res.send(err));
+  }
+
+  static addToCart(req, res) {
+    const {productId:ProductId} = req.params;
+    const { userId } = req.session;
+    if(!userId) return res.redirect ('/login?error=You session is ended, please login again.')
+    Cart.findOne({where: {UserId:+userId}})
+      .then(getCart => {
+        const {id} = getCart
+        console.log(id, +ProductId)
+        return CartProduct.create({ProductId : +ProductId, CartId:id})
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => res.send(err))
+  }
+
+  static getCart(req, res) {
+    let {errors} = req.query
+    const { userId, role } = req.session;
+    if(!userId) return res.redirect ('/login?error=You session is ended, please login again.')
+    Cart.findOne({where: {UserId:+userId}, include:[{model:Product}, {model:User, include:{model:UserDetail}}]})
+      .then(getCart => res.render('cart', {getCart, userId, role, errors, currencyFormat}))
+      .catch(err => res.send(err))
+  }
+
+  static getPayCart(req, res) {
+    const { userId, role } = req.session;
+    let {totalPrice} = req.query
+    Cart.findOne({where: {UserId:+userId}, include:[{model:Product, order:[['name']]}, {model:User, include:{model:UserDetail}}]})
+    .then(getCart => {
+      let {User, Products} = getCart.dataValues
+      let {UserDetail} = User.dataValues
+      let {balance} = UserDetail.dataValues
+      console.log(`========================================`)
+      let arrIdProduct = Products.map(el => {
+        let {id} = el.dataValues
+        return id
+      })
+      if(+totalPrice === 0) throw {name:`custom`, msg: `Nothing to buy, add product first`}
+      else if(balance >= +totalPrice){
+        console.log(`HEREEEEEEEEEE`)
+         return UserDetail.increment({balance: -totalPrice})
+      }
+      else {
+        throw {name:`custom`, msg: `Balance not enough`}
+      }
+    })
+    .then(arr => {
+      console.log(`Deleteeeee`)
+      let {cartId} = req.params
+      return CartProduct.destroy({
+        where: {
+          CartId: +cartId
+        }
+      })
+    })
+    .then(success=> res.redirect(`/Customer/${userId}/cart`))
+    .catch(err => {
+      if(err.name === `custom`) return res.redirect(`/customer/${userId}/cart?errors=${err.msg}`)
+      else res.send(err)})
   }
 }
 
