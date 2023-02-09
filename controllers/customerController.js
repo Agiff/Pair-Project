ec51a83db914451e7c66cb61777ebf747ad4e5c5
mfahriferdiansyah <xkadzuvic@gmail.com>
2023-02-09 19:18:14 +0700

    +transaction : progress

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,9 +1,20 @@
 const { User, UserDetail, Product, Category } = require('../models');
 const { currencyFormat } = require('../helper');
+const { isCustomer } = require('../middlewares');
 
 class CustomerController {
   static productDetail(req, res) {
     res.send('asd')
+  }
+
+  static getTransaction(req, res) {
+    console.log(req.session.role)
+    let include = {model:Product}
+    let where = {id : req.session.userId}
+    console.log(where)
+    User.findByPk({include, where})
+      .then(getTransaction => res.render('transaction', {getTransaction}))
+      .catch(err => res.send(err))
   }
 }
 
--- routers/customer.js
+++ routers/customer.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const CustomerController = require('../controllers/customerController');
 
 router.get('/product/:productId', CustomerController.productDetail);
+router.get('/transaction', CustomerController.getTransaction)
 
 module.exports = router;
