29ff6f4ccb735bfc5e8487a410c75fb09f766dfc
mfahriferdiansyah <xkadzuvic@gmail.com>
2023-02-09 10:22:06 +0700

    +fix migration

--- migrations/20230208125425-create-product.js
+++ migrations/20230208125425-create-product.js
@@ -30,7 +30,7 @@ module.exports = {
         allowNull: false
       },
       brand: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         allowNull: false
       },
       createdAt: {
